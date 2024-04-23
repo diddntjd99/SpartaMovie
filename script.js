f8ae0709e3a4929c0c2ed347491bcb8f25c8639b
diddntjd99 <diddntjd99@gmail.com>
2024-04-23 17:28:27 +0900

    검색 시 movie card content 추가

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//TMDB api 키값
 const options = {
     method: 'GET',
     headers: {
@@ -8,10 +9,11 @@ const options = {
 
 //페이지가 로딩되면 실행
 window.addEventListener("load", function () {
+    //포커스
     document.getElementById("searchTitle").focus();
 
     document.getElementById("searchTitle").addEventListener("keypress", function (event) {
-        // Enter 키인 경우
+        //Enter 키인 경우
         if (event.key === "Enter") {
             searchMovie();
         }
@@ -20,44 +22,69 @@ window.addEventListener("load", function () {
 
 function searchMovie() {
     let fetchMovie = (search) => {
-        let movieArr = [];
-
-        // 가져올 데이터의 URL, search 단어를 통해 해당 제목을 가진 영화만 검색
+        //가져올 데이터의 URL, search 단어를 통해 해당 제목을 가진 영화만 검색
         const url = 'https://api.themoviedb.org/3/search/movie?query=' + search + '&include_adult=false&language=en-US&page=1';
 
-        // Fetch API를 사용하여 데이터 가져오기
+        //Fetch API를 사용하여 데이터 가져오기
         fetch(url, options).then(response => {
             // 서버 응답 확인
             if (!response.ok) {
                 throw new Error('네트워크 상태가 좋지 않습니다.');
             }
-            // JSON 형식으로 변환하여 반환
+            //JSON 형식으로 변환하여 반환
             return response.json();
         }).then(data => {
-            // 가져온 데이터 출력
-            // data['results'][반복문][필요한 데이터 이름('title', 'overview', 'poster_path', 'vote_average')]
+            //가져온 데이터 출력
+            //data['results'][반복문][필요한 데이터 이름('title', 'overview', 'poster_path', 'vote_average')]
             console.log('data:', data);
 
             data['results'].forEach(element => {
-                movieArr.push(element);
-
                 console.log(element['title']);
                 console.log(element['overview']);
                 console.log(element['poster_path']);
                 console.log(element['vote_average']);
                 console.log('--------------------------------------------------------------');
+
+                // 새로운 div 요소 생성
+                let movieCard = document.createElement('div');
+                movieCard.classList.add('row', 'row-cols-1', 'row-cols-md-5', 'g-4');
+                movieCard.id = element['id'];
+
+                // 이미지 요소 생성 및 속성 설정
+                let img = document.createElement('img');
+                img.classList.add('poster');
+                img.src = 'https://image.tmdb.org/t/p/w500' + element['poster_path'];
+                movieCard.appendChild(img);
+
+                // 이름 요소 생성 및 텍스트 설정
+                let title = document.createElement('p');
+                title.classList.add('movieName');
+                title.textContent = `이름: ${element['title']}`;
+                movieCard.appendChild(title);
+
+                // 설명 요소 생성 및 텍스트 설정
+                let explanation = document.createElement('p');
+                explanation.classList.add('explanation');
+                explanation.textContent = `설명: ${element['overview']}`;
+                movieCard.appendChild(explanation);
+
+                // 평점 요소 생성 및 텍스트 설정
+                let rating = document.createElement('p');
+                rating.classList.add('rating');
+                rating.textContent = `평점: ${element['vote_average']}`;
+                movieCard.appendChild(rating);
+
+                document.getElementById('movieContent').appendChild(movieCard);
             });
         }).catch(error => {
-            // 오류 처리
+            //오류 처리
             console.error('Fetch 오류:', error);
             alert('Fetch 오류: ' + error.message);
         });
-
-        return movieArr;
     };
     //검색할 단어 추출 및 삭제
     let searchTitle = document.getElementById('searchTitle').value;
     document.getElementById('searchTitle').value = '';
 
-    let movieArr = fetchMovie(searchTitle);
+    fetchMovie(searchTitle);
 }
