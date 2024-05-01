4808c05976e537e0bf6e09afd83f155198fc5476
komiharuu <biomilky3@gmail.com>
2024-05-01 19:21:01 +0900

    details

---
+++ detail_ui.js
@@ -0,0 +1,31 @@
+
+
+function append(){
+    const title = document.createElement("p");
+    title.classList.add("movieTitle");
+    title.textContent = `${element.title}`;
+    const img = document.createElement("img");
+    img.classList.add("poster");
+    img.src = "https://image.tmdb.org/t/p/w500" + element.poster_path;
+    const text = document.createElement("p");
+    title.classList.add("explantion");
+    title.textContent = `${element.overview}`;
+document.getElementById("imagecontent").getAttribute(movieCard);}
+
+// 영화 카드 양식 가져오기?(이미지,사진,내용) 창에 나타내야되기 때문에 append를 사용했는데, 맞는지 모르겠습니다. 이 부분은 도움이 필요합니다.
+
+
+
+
+
+window.addEventListener("load", function () {
+    alert('상세 페이지 입니다. 환영합니다')
+
+    this.document.title = receivedData.title;
+ });
+
+//  상세 페이지 접속을 환영한다.
+
+
+ 
+
